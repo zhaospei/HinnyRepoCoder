5eb998e4c3431b71a35ed33d60892aeddd6e77da
The One <dungbuit1k28@gmail.com>
2024-08-03 11:32:45 +0700

    hsjkadh

--- deepseek_inference.py
+++ deepseek_inference.py
@@ -92,7 +92,7 @@ class CodeGen:
 
 
 if __name__ == '__main__':
-    file_path = 'prompt_no_em_type_method_SN_k20_t8000.jsonl'
+    file_path = 'prompt_no_em_type_method_SN_k20_t8000_lcontext_havepath.jsonl'
     tiny_codegen = 'deepseek-ai/deepseek-coder-6.7b-base'
 
     cg = CodeGen(tiny_codegen, batch_size=1)
